9013803d3f41c9324dfff3b5eb04a99a4b031040
George Keith <ggkeithv@gmail.com>
2015-07-24 16:32:36 -0500

    slider will start over at first image after it reaches the end

--- js/slider.js
+++ js/slider.js
@@ -10,6 +10,11 @@ function startSlider() {
 	count = $("#slider > img").size();
 
 	loop = setInterval(function() {
+		if (sliderNext > count) {
+			sliderNext = 1;
+			sliderInt = 1;
+		}
+
 		$('#slider > img').fadeOut(300);
 		$('#slider > img#' + sliderNext).fadeIn(300);
 
